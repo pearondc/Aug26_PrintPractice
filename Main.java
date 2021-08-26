769ba8a723866ee9bd6a0e57965da3f02bf2a4d9
pearondc <89395893+pearondc@users.noreply.github.com>
2021-08-26 15:23:23 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,24 @@
+class Main {
+  public static void main(String[] args) {     
+    System.out.println("HIHI      HIHI\tHIHIHIHIHIHI");
+    System.out.println("HIHI      HIHI\tHIHIHIHIHIHI");
+    System.out.println("HIHI      HIHI\t    HIHI");
+    System.out.println("HIHIHIHIHIHIHI\t    HIHI");
+    System.out.println("HIHIHIHIHIHIHI\t    HIHI");
+    System.out.println("HIHI      HIHI\t    HIHI");
+    System.out.println("HIHI      HIHI\tHIHIHIHIHIHI");
+    System.out.println("HIHI      HIHI\tHIHIHIHIHIHI");
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    System.out.println("111111111111111");
+    System.out.println("11  1111111  11");
+    System.out.println("111111111111111");
+    System.out.println("1111111 1111111");
+    System.out.println("111111   111111");
+    System.out.println("11 111111111 11");
+    System.out.println("111 1111111 111");
+    System.out.println("1111       1111");
+    System.out.println("111111111111111");
+  }
+}
